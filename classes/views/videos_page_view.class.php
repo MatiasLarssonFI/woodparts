d9c02ce19016c037e48022b0c21e38e6dbc08530
Matias Larsson <matias.larsson@gmail.com>
2016-05-02 21:57:10 +0300

    Added videos page view

---
+++ classes/views/videos_page_view.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Views;
+
+require_once(dirname(__FILE__) . "/abstract_view.class.php");
+require_once(dirname(__FILE__) . "/../ui_text_storage.class.php");
+require_once(dirname(__FILE__) . "/../video_factory.class.php");
+
+class VideosPageView extends AbstractView {
+    protected function get_required_params() {
+        return array("selected_video");
+    }
+    
+    
+    protected function get_template_name() {
+        return "videos_page.html";
+    }
+    
+    
+    protected function get_view_data(array $params) {
+        $text_storage = \UITextStorage::get();
+        return array(
+            "strings" => array(
+                "page_title" => $text_storage->text("VIDEOS_PAGE_TITLE")
+            ),
+            "videos" => \VideoFactory::get()->get_videos_page_videos(),
+            "selected_video" => \VideoFactory::get()->get_videos_page_video((int)$params["selected_video"]),
+            "lang" => $text_storage->get_language()
+        );
+    }
+}
