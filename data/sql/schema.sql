e28c64b72f7bdfe0bac76c37c15a7f0395d3ab08
Matias Larsson <matias.larsson@gmail.com>
2016-05-06 23:39:51 +0300

    Updated DB schema: video_file table

--- data/sql/schema.sql
+++ data/sql/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 04, 2016 at 06:26 PM
+-- Generation Time: May 06, 2016 at 11:38 PM
 -- Server version: 5.5.49-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.16
 
@@ -116,13 +116,38 @@ CREATE TABLE IF NOT EXISTS `videos_page_video` (
   `name` text COLLATE utf8_swedish_ci NOT NULL,
   `description` text COLLATE utf8_swedish_ci NOT NULL,
   `thumb_url` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
-  `video_url` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
-  `mime_subtype` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
   `is_published` tinyint(1) NOT NULL DEFAULT '1',
   `time_edited` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `time_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video_file`
+--
+
+CREATE TABLE IF NOT EXISTS `video_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `videos_page_video_id` int(11) NOT NULL,
+  `video_url` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `mime_subtype` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `time_edited` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `time_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `videos_page_video_id` (`videos_page_video_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci AUTO_INCREMENT=5 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `video_file`
+--
+ALTER TABLE `video_file`
+  ADD CONSTRAINT `video_file_ibfk_1` FOREIGN KEY (`videos_page_video_id`) REFERENCES `videos_page_video` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
